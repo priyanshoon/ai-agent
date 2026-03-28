a2182c3e928fa32c3f069a9b07d2043b793fb453
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2026-03-28 22:55:39 +0530

    batman

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    try:
+        abs_path = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_path, directory))
+
+        valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        items = os.listdir(target_dir)
+
+        result = ""
+        for item in items:
+            result += f"- {item}: file_size={os.path.getsize(os.path.join(target_dir, item))} bytes, is_dir={os.path.isdir(os.path.join(target_dir, item))}\n"
+
+        return result
+    except Exception as e:
+        print("Error:", e)
